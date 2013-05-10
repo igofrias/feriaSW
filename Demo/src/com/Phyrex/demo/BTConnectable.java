a4299b6ab52d5e280015f3fa4633ea2aebb08111
Celeste Bertin <celbertin@gmail.com>
2013-05-10 13:14:36 -0400

    Demo del proyecto

---
+++ Demo/src/com/Phyrex/demo/BTConnectable.java
@@ -0,0 +1,16 @@
+/**
+ *   (Changes from original are) Copyright 2010 Guenther Hoelzl, Shawn Brown
+ * 
+ * (original work is) Copyright (C) 2009 The Android Open Source Project
+**/
+
+package com.Phyrex.demo;
+
+public interface BTConnectable {
+
+    /**
+     * @return true, when currently pairing 
+     */
+    public boolean isPairing();
+
+}
