94082d9af98188518c5753f8112fba37eca5f7d4
Rodrigo Frias <hieru.yuki@gmail.com>
2013-10-17 15:40:09 -0300

    "Vader.c" muestra ojos parpadeando y sonido de Vader

---
+++ Proyecto/Robot/Vader.c
@@ -0,0 +1,26 @@
+task Vader();
+task OpenCloseEyes();
+
+task main(){
+	StartTask(OpenCloseEyes);
+	StartTask(Vader);
+	wait1Msec(5000);
+	StopAllTasks();
+	return;
+}
+
+task Vader(){
+	while(true){
+		PlaySoundFile("Vader.rso");
+		wait1Msec(1500);
+	}
+}
+
+task OpenCloseEyes(){
+	while(true){
+		nxtDisplayRICFile(0, 0, "OpenEyes.ric");
+		wait1Msec(2000);
+		nxtDisplayRICFile(0, 0, "CloseEyes.ric");
+		wait1Msec(200);
+	}
+}
