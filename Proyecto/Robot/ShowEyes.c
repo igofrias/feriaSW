df62615e83a2ab21f146baaeb5480d80c028620a
Rodrigo Frias <hieru.yuki@gmail.com>
2013-10-17 15:30:05 -0300

    Modificado el codigo de ojos. Usa imagenes en vez de crear ojos por funciones

--- Proyecto/Robot/ShowEyes.c
+++ Proyecto/Robot/ShowEyes.c
@@ -1,47 +1,17 @@
-#define X_BigCircleSup	5
-#define Y_BigCircleSup	51
-#define X_BigCircleInf	44
-#define Y_BigCircleInf	12
-#define X_SmallCircleSup	25
-#define Y_SmallCircleSup	46
-#define X_SmallCircleInf	39
-#define Y_SmallCircleInf	32
-#define X_EllipseSup		9
-#define	Y_EllipseSup		26
-#define X_EllipseInf		19
-#define	Y_EllipseInf		19
-#define	DistanceLR		50
-
-task ShowEyes(){
-	int X_BCS,X_BCI;
-	int X_SCS,X_SCI;
-	int X_ES,X_EI;
-
-	//Position for Right Eyes
-	//Right Big Circle
-	X_BCS=X_BigCircleSup+DistanceLR;
-	X_BCI=X_BigCircleInf+DistanceLR;
-
-	//Right Small Circle
-	X_SCS=X_SmallCircleSup+DistanceLR;
-	X_SCI=X_SmallCircleInf+DistanceLR;
-
-	//Right Ellipse
-	X_ES=X_EllipseSup+DistanceLR;
-	X_EI=X_EllipseInf+DistanceLR;
-
-	//Display Right Eye
-	nxtFillEllipse(X_BCS,Y_BigCircleSup,X_BCI,Y_BigCircleInf);
-	nxtEraseEllipse(X_SCS,Y_SmallCircleSup,X_SCI,Y_SmallCircleInf);
-	nxtEraseEllipse(X_ES,Y_EllipseSup,X_EI,Y_EllipseInf);
-	//Display Left Eye
-	nxtFillEllipse(X_BigCircleSup,Y_BigCircleSup,X_BigCircleInf,Y_BigCircleInf);
-	nxtEraseEllipse(X_SmallCircleSup,Y_SmallCircleSup,X_SmallCircleInf,Y_SmallCircleInf);
-	nxtEraseEllipse(X_EllipseSup,Y_EllipseSup,X_EllipseInf,Y_EllipseInf);
-}
+task OpenCloseEyes();
 
 task main(){
-	StartTask(ShowEyes);
-	wait10Msec(500);
-	StopTask(ShowEyes);
+	StartTask(OpenCloseEyes);
+	wait1Msec(10000);
+	StopTask(OpenCloseEyes);
+	return;
+}
+
+task OpenCloseEyes(){
+	while(true){
+		nxtDisplayRICFile(0, 0, "OpenEyes.ric");
+		wait1Msec(2000);
+		nxtDisplayRICFile(0, 0, "CloseEyes.ric");
+		wait1Msec(200);
+	}
 }
