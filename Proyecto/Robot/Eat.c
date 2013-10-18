e91d4637daec4ff007795af6b371d5ccaf1f5464
Rodrigo Frias <hieru.yuki@gmail.com>
2013-10-17 22:08:53 -0300

    "Eat.c" ahora muestra ojos felices y mueve la cabeza

--- Proyecto/Robot/Eat.c
+++ Proyecto/Robot/Eat.c
@@ -1,9 +1,16 @@
+#pragma config(Motor, motorB,  headMotor, tmotorNXT, PIDControl)
+
 task EatSound();
+task HappyEyes();
+task MoveHead();
 
 task main(){
+	StartTask(HappyEyes);
 	StartTask(EatSound);
+	StartTask(MoveHead);
 	wait1Msec(5000);
-	StopTask(EatSound);
+
+	StopAllTasks();
 	return;
 }
 
@@ -12,4 +19,17 @@ task EatSound(){
 		PlaySoundFile("nom1.rso");
 		wait1Msec(1500);
 	}
+}
+
+task HappyEyes(){
+	while(true){
+		nxtDisplayRICFile(0, 0, "HappyEyes.ric");
+	}
+}
+
+task MoveHead(){
+	motor[headMotor]=25;
+	wait10Msec(40);
+	motor[headMotor]=-25;
+	wait10Msec(40);
 }
