09ce1757c6aefd33330f9582b2257d23d407b5d5
Celeste Bertin <celbertin@gmail.com>
2013-12-18 16:11:52 -0300

    BTService funciona!

--- Viper/Proyecto/src/com/Phyrex/VIPeR/BTService.java
+++ Viper/Proyecto/src/com/Phyrex/VIPeR/BTService.java
@@ -494,7 +494,7 @@ public class BTService extends Service implements BTConnectable{
          public void handleMessage(Message myMessage) {
          	int messageType = myMessage.getData().getInt("message");
              switch (messageType) {
-            switch (messageCode) {
+            
                  case BTCommunicator.DISPLAY_TOAST:
                      //showToast(myMessage.getData().getString("toastText"), Toast.LENGTH_SHORT);
                      break;
